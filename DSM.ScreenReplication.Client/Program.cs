dba668ca8198f1c4a934795ba1eac992fff5c23e
dsantos1998 <danielsantosmundina2@gmail.com>
2024-08-28 17:28:21 +0200

    Agregar archivos de proyecto.

---
+++ DSM.ScreenReplication.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSM.ScreenReplication.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const int port = 12345; // Puerto que usarás para la comunicación
+            var listener = new TcpListener(IPAddress.Any, port);
+
+            listener.Start();
+            Console.WriteLine("Esperando conexión...");
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                Console.WriteLine("Cliente conectado.");
+
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.UTF8.GetBytes("Hola desde la aplicación de consola");
+                await stream.WriteAsync(data, 0, data.Length);
+
+                client.Close();
+            }
+        }
+    }
+}
---
+++ DSM.ScreenReplication.GUI/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DSM.ScreenReplication.GUI
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int Port = 12345; // Debe coincidir con el puerto de la aplicación de consola
+        private const string ServerIp = "127.0.0.1"; // La dirección IP del servidor (localhost)
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReceiveData();
+        }
+
+        private async void ReceiveData()
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    await client.ConnectAsync(ServerIp, Port);
+                    NetworkStream stream = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                    var receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    tbkTest.Text = $"Datos recibidos: {receivedData}";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
